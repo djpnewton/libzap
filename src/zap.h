6ed0a382087f80fd07581248d0e6204c3059c263
Daniel Newton <djpnewton@gmail.com>
2018-07-26 14:24:56 +1200

    add seed_to_address method to jni

--- src/zap.h
+++ src/zap.h
@@ -1,7 +1,13 @@
 #ifndef __ZAP_H__
 #define __ZAP_H__
 
-int lzap_version();
-void lzap_seed_to_address(const unsigned char *key, unsigned char network_byte, unsigned char *output);
+#ifdef __cplusplus
+extern "C" {
+#endif
+    int lzap_version();
+    void lzap_seed_to_address(const unsigned char *key, unsigned char network_byte, unsigned char *output);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- src/zap_jni.c
+++ src/zap_jni.c
@@ -1,8 +1,18 @@
 #include <jni.h>
+#include <string.h>
 
 #include "zap.h"
 
-int Java_com_djpsoft_zap_plugin_zap_1jni_version()
+JNIEXPORT jint JNICALL Java_com_djpsoft_zap_plugin_zap_1jni_version(JNIEnv* env, jobject thiz)
 {
     return lzap_version();
+}
+
+JNIEXPORT jstring JNICALL Java_com_djpsoft_zap_plugin_zap_1jni_seed_1to_1address(
+    JNIEnv* env, jobject thiz, jstring key, jchar network_byte)
+{
+    char* c_key = strdup((*env)->GetStringUTFChars(env, key, 0));
+    char output[1024];
+    lzap_seed_to_address(c_key, network_byte, output);
+    return (*env)->NewStringUTF(env, output);
 }
